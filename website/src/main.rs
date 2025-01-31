662bfd92231b6c6265005894bedad876bcc081c6
alfa-ryano <ary506@york.ac.uk>
2025-01-31 17:24:28 +0700

    Add Tailwind CSS setup and static files; remove legacy animations and HTML template

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,36 +1,14 @@
 use actix_files as fs;
-use actix_web::{web, App, HttpServer, HttpResponse, Result};
-use askama::Template;
-
-// Askama template for rendering
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate<'a> {
-    title: &'a str,
-    header: &'a str,
-}
-
-async fn index() -> Result<HttpResponse> {
-    let template = IndexTemplate {
-        title: "Bolio",
-        header: "Welcome to Bolio",
-    };
-
-    let body = template.render().unwrap();
-    Ok(HttpResponse::Ok().content_type("text/html").body(body))
-}
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("🚀 Server running at http://127.0.0.1:50785");
-
     HttpServer::new(|| {
         App::new()
-            // Serve static files from /static
-            .service(fs::Files::new("/static", "./static").show_files_listing())
-            .route("/", web::get().to(index))
+            // Serve the `index.html` file as the root
+            .service(fs::Files::new("/", "./static").index_file("index.html"))
     })
-    .bind("127.0.0.1:50785")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
